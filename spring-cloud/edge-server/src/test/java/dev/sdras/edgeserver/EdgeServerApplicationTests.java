3703a041ea4ce080390e5ccd06b3d1b20f84c2c8
Esdras <bzr4@icloud.com>
2025-09-15 10:06:49 -0300

    Ajustes monorepo e conclusao de edge-server.

---
+++ spring-cloud/edge-server/src/test/java/dev/sdras/edgeserver/EdgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sdras.edgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
